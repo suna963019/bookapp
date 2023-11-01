c2d63baae8654b50bb7a974beceadc49353189eb
suna963019 <sunahara20011205@gmail.com>
2023-11-01 14:22:34 +0900

    seeder追加

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -15,7 +15,19 @@ class BooksTableSeeder extends Seeder
         $param=[
             'name'=>'不思議の国のアリス',
             'other'=>'ルイス・キャロル',
-            'price'=>'4000',
+            'price'=>4000,
+        ];
+        DB::table('books')->insert($param);
+        $param=[
+            'name'=>'人魚姫',
+            'other'=>'ハンス・クリスチャン・アンデルセン',
+            'price'=>300,
+        ];
+        DB::table('books')->insert($param);
+        $param=[
+            'name'=>'test',
+            'other'=>'tester',
+            'price'=>1000,
         ];
         DB::table('books')->insert($param);
     }
